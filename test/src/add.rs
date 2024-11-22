1f2d6769b2864182ea0151bf611642540a721267
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2024-11-21 16:19:07 -0800

    clippy

--- test/src/add.rs
+++ test/src/add.rs
@@ -6,14 +6,14 @@ isf_macro::isf!("isf/testcase/add.isf");
 fn add() -> Result<(), anyhow::Error> {
     let a = Add::parse_machine(0b00000101_00000100_00000011_10000010).unwrap();
     assert_eq!(a.get_opcode(), 2);
-    assert_eq!(a.get_sign_extend(), false);
+    assert!(!a.get_sign_extend());
     assert_eq!(a.get_dst(), 3);
     assert_eq!(a.get_src1(), 4);
     assert_eq!(a.get_src2(), 5);
 
     let ap = Add::parse_assembly("add r3 r4 r5").unwrap();
     assert_eq!(ap.get_opcode(), 2);
-    assert_eq!(a.get_sign_extend(), false);
+    assert!(!a.get_sign_extend());
     assert_eq!(ap.get_dst(), 3);
     assert_eq!(ap.get_src1(), 4);
     assert_eq!(ap.get_src2(), 5);
--- test/src/add_field_opt.rs
+++ test/src/add_field_opt.rs
@@ -8,13 +8,13 @@ fn add() -> Result<(), anyhow::Error> {
     assert_eq!(a.get_a(), 2);
     assert_eq!(a.get_b(), 1);
     assert_eq!(a.get_src1_sel(), 0);
-    assert_eq!(a.src1_sel_is_set(), false);
+    assert!(!a.src1_sel_is_set());
 
     let a = AddOptField::parse_assembly("add r4 r3.7 2 1").unwrap();
     assert_eq!(a.get_a(), 2);
     assert_eq!(a.get_b(), 1);
     assert_eq!(a.get_src1_sel(), 7);
-    assert_eq!(a.src1_sel_is_set(), true);
+    assert!(a.src1_sel_is_set());
 
     Ok(())
 }
