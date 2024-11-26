60c7c86c8f623b46505ca8c7d500d6cb78d4e94b
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2024-11-25 20:59:31 -0800

    add license

--- isf-macro/src/lib.rs
+++ isf-macro/src/lib.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 use isf::{codegen::generate, parse::parse, spec::form_spec};
 use proc_macro::TokenStream;
 use std::fs::read_to_string;
